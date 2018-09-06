2e1f8dc88f9023f3a15dd3b60b9ba7e3a60dd8e8
MPriest <miche.priest@gmail.com>
2018-09-05 19:01:10 -0700

    added POST route that removes url, after, gets redirects to /urls

--- express_server.js
+++ express_server.js
@@ -10,28 +10,39 @@ const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
+// body parser for the POST route for the delete button
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.get("/urls.json", (req, res) => {
     res.send(urlDatabase);
 });
-
+// creates title
 app.get("/urls", (req, res) => {
     const templateVars = { urls: urlDatabase, title: "TinyApp" };
     res.render("urls_index", templateVars);
 });
-
-// app.get("/michelle", (req, res) => {
-//     const anObject = { 
-//         ourName : "michelle",
-//         ourPoliticalViews : "trump is the best"
-//     };
-//     res.render("urls_test", anObject);
-// });
+// get shortURL
 app.get("/urls/:short", (req, res) => {
     const templateVars = { shortURL: req.params.id };
     res.render("urls_show", templateVars);
 });
+// delete url
+// app.post("/urls/:id/delete", (req,res)=>{
+//     const targetURL = req.params.url;
+//     delete urlDatabase.;
+// });
 
+// POST http://localhost:8080/api/users
+// parameters sent with 
+app.post('/urls/:id/delete', function(req, res) {
+    let urls = req.params.id;
+    console.log(urls);
+    delete urlDatabase[urls];
+    console.log(urlDatabase);
+    res.redirect("/urls");
+});
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
 });
