9be7a2308ac0f869b8737d99938a16f15f439502
michepriest <miche.priest@gmail.com>
2018-09-13 09:46:57 +0000

    halfway through user verification

--- express_server.js
+++ express_server.js
@@ -1,16 +1,14 @@
 'use strict'
-
+// Dependencies
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const app = express();
-const PORT = 8080;
+
 
 app.set('view engine', 'ejs');
-
-// Dependencies
-const bodyParser = require('body-parser');
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
-const cookieSession = require('cookie-session');
 app.use(cookieSession({
   name: 'session',
   keys: ['secret'],
@@ -18,6 +16,7 @@ app.use(cookieSession({
 }));
 
 // URL and userDb
+const PORT = 8080;
 const urlDatabase = {
   'b2xVn2': 'https://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com',
@@ -44,8 +43,8 @@ const userDb = {
 };
 
 // Used Matt's code...he said he got the base of it from the top response by csharptest https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript 
+// Solution from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
 function generateRandomString(digits) {
-  //Solution from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
   let text = '';
   let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (let i = 0; i <= digits; i++) {
@@ -54,135 +53,157 @@ function generateRandomString(digits) {
   return text;
 };
 
-app.get('/urls.json', (req, res) => {
-  res.send(urlDatabase);
+app.get('/urls.json', (request, response) => {
+  response.send(urlDatabase);
 });
 
 // list all the URLs (shortened, with delete links)
-app.get('/urls', (req, res) => {
+app.get('/urls', (request, response) => {
 // TROUBLESHOOTING: THE ABOVE IS OKAY  
-  let user_id = req.session.user_id; 
+  let user_id = request.session.user_id; 
   // ABOVE NEEDS TROUBLESHOOTING 000000000000000000
   let templateVars = { urls: urlDatabase, title: 'TinyApp', user: user_id }; // TODO: MAKE DYNAMIC!!!
   // TROUBLESHOOTING: let templateVars = { urls: urlDatabase IS OKAY 000000000000
-  res.render('urls_index', templateVars);
+  response.render('urls_index', templateVars);
   // TROUBLESHOOTING: THE ABOVE IS OKAY
 });
 
 // show the user the form to create a new short url
-app.get('/urls/new', (req, res) => {
+app.get('/urls/new', (request, response) => {
 // TROUBLESHOOTING: THE ABOVE IS OKAY  
-  let user_id = req.session.user_id;
+  let user_id = request.session.user_id;
 // ?????????????????????????????????????????????????????????????????
   let templateVars = { user: user_id };
-  res.render('urls_new', templateVars);
-  // TROUBLESHOOTING: res.render('urls_new' IS OKAY
+  response.render('urls_new', templateVars);
+  // TROUBLESHOOTING: response.render('urls_new' IS OKAY
 });
 
 // get shortURL NEEDS MORE
-app.get('/urls/:id', (req, res) => {
+app.get('/urls/:id', (request, response) => {
 // TROUBLESHOOTING: THE ABOVE IS OKAY  
-  let user_id = req.session.user_id;
+  let user_id = request.session.user_id;
   // ABOVE NEEDS TROUBLESHOOTING 000000000000000000
-  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id], user: user_id };
-  // TROUBLESHOOTING: let templateVars = { shortURL: req.params.id, IS OKAY 0000000000
-  res.render('urls_show', templateVars);
+  let templateVars = { shortURL: request.params.id, longURL: urlDatabase[request.params.id], user: user_id };
+  // TROUBLESHOOTING: let templateVars = { shortURL: request.params.id, IS OKAY 0000000000
+  response.render('urls_show', templateVars);
 });
 
 // Creates a registration page
-app.get('/register', (req, res) => {
+app.get('/register', (request, response) => {
   let templateVars = { title: "Register"}
-  //let user_id = req.body.user_id;
-  //let password = req.body.password;
-  res.render('register', templateVars);
+  //let user_id = request.body.user_id;
+  //let password = request.body.password;
+  response.render('register', templateVars);
 });
 // ?????????????????????????????????????????????????????????????????
 
 // login page
-app.get('/login', (req, res) => {
+app.get('/login', (request, response) => {
   let templateVars = { title: 'Login'}
-  let user_id = req.body.user_id;
-  let password = req.body.password;
-  res.render('login', templateVars);
+  let user_id = request.body.user_id;
+  let password = request.body.password;
+  response.render('login', templateVars);
 });
 // ?????????????????????????????????????????????????????????????????
 
 // public-facing link that turns magically into the full longURL
-app.get('/u/:shortURL', (req, res) => {
-  let shortURL = req.params.shortURL;
+app.get('/u/:shortURL', (request, response) => {
+  let shortURL = request.params.shortURL;
   let longURL = urlDatabase[shortURL];
-  res.redirect(longURL);
+  response.redirect(longURL);
 });
 // TROUBLESHOOTING: THE ABOVE IS OKAY 
 
 // registration
-app.post('/register', (req, res) => {
-  let user_id = req.body.email;
-  let password = req.body.password;
+app.post('/register', (request, response) => {
+
+  let user_id = request.body.email;
+  let password = request.body.password;
   let userId = generateRandomString(6);
   userDb[userId] = {id: userId, user: user_id, password: password};
   // console.log(user, password);
-  // res.cookie(username, req.body.username);
-  req.session.user_id = userId;
-  res.redirect('/urls');
+  // response.cookie(username, request.body.username);
+  request.session.user_id = userId;
+  response.redirect('/urls');
 });
 // ?????????????????????????????????????????????????????????????????
 
 // Login
-app.post('/login', (req, res) => {
-  let user_id = req.body.email;
-  let password = req.body.password;
-  let userId = generateRandomString(6);
-  userDb[userId] = {id: userId, user: user_id, password: password};
-  console.log(user_id, password);
-  // res.cookie(username, req.body.username);
-  req.session('user_id', user_id);
-  req.session('user_id', userId);
-  res.redirect('/urls');
+app.post('/login', (request, response) => {
+  console.log(request)
+  console.log('-----------------------------------------------------')
+  console.log('-----------------------------------------------------')
+  console.log('-----------------------------------------------------')
+  console.log(request.body)
+
+  // verify user_id === userDb, if true, next step | if false, send error
+  let user_id = request.body.email;
+  for (let key in userDb) {
+    userDb[key].email
+    console.log(key)
+  }
+  
+  user_id === userDb;
+
+  // verify correct password, if true, next step | if false, send error
+  // set user_id cookie on successfull login
+  
+  // let user_id = request.body.email;
+  // let password = request.body.password;
+  // let userId = generateRandomString(6);
+  // userDb[userId] = {id: userId, user: user_id, password: password};
+  // console.log(user_id, password);
+  // // response.cookie(username, request.body.username);
+  // request.session('user_id', user_id);
+  // request.session('user_id', userId);
+  // response.redirect('/urls');
 });
-// let user_id = req.body.user_id;
-// req.session(user_id, req.body.user_id);
+
+//  app.post('/login', (request, response) => {
+//    let user_id = request.body.user_id;
+//    request.session(user_id, request.body.user_id);
+//    response.redirect('/urls');
 // The above two lines may or may not be needed.
 
 // TROUBLESHOOTING ABOVE D2D3 COOKIES IN EXPRESS TASK 3
 
 // the login form is in the _header partial
-app.post('/logout', (req, res) => {
-  let user_id = req.body.user_id;
-  res.clearCookie(user_id, req.body.user_id);
-  res.redirect('/urls');
+app.post('/logout', (request, response) => {
+  let user_id = request.body.user_id;
+  response.clearCookie(user_id, request.body.user_id);
+  response.redirect('/urls');
 });
 
 // Actually does the URL-shortening:
 //   * generates a random url
 //   * adds or appends shortURL into the database
 //   * sends them somewhere.  where?  drat!
-app.post('/urls', (req, res) => {
+app.post('/urls', (request, response) => {
   let shortURL = generateRandomString(6); 
   // console.log(shortURL);
-  console.log(req.body.longURL);
-  urlDatabase[shortURL] = req.body.longURL;
+  console.log(request.body.longURL);
+  urlDatabase[shortURL] = request.body.longURL;
   // console.log(urlDatabase);
   // TODO: we're ghosting on the user's browser.  stop ghosting, tell them where to go next.
 });
 // TROUBLESHOOTING: ABOVE SHOULD BE OKAY, BUT NOT SURE 0000000000000000000
 
 // route to /urls after updating short url
-app.post('/urls/:id', (req, res) => {
-  console.log(req.body.longURL);
-  urlDatabase[req.params.id] = req.body.longURL;
+app.post('/urls/:id', (request, response) => {
+  console.log(request.body.longURL);
+  urlDatabase[request.params.id] = request.body.longURL;
   console.log(urlDatabase);
-  res.redirect('/urls');
+  response.redirect('/urls');
 });
 // TROUBLESHOOTING: THE ABOVE IS OKAY 
 
 // parameters sent with 
-app.post('/urls/:id/delete', function(req, res) {
-  let urlId = req.params.id;
+app.post('/urls/:id/delete', function(request, response) {
+  let urlId = request.params.id;
   // console.log(urlId);
   delete urlDatabase[urlId];
   // console.log(urlDatabase);
-  res.redirect('/urls');
+  response.redirect('/urls');
 });
 // TROUBLESHOOTING: THE ABOVE IS OKAY 
 
