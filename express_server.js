1726809c5bf120eacf3bcc77f519a5fad55f126d
michepriest <miche.priest@gmail.com>
2018-09-13 13:20:20 +0000

    updating 'user_id' with 'email' and debugging

--- express_server.js
+++ express_server.js
@@ -59,52 +59,37 @@ app.get('/urls.json', (request, response) => {
 
 // list all the URLs (shortened, with delete links)
 app.get('/urls', (request, response) => {
-// TROUBLESHOOTING: THE ABOVE IS OKAY  
-  let user_id = request.session.user_id; 
-  // ABOVE NEEDS TROUBLESHOOTING 000000000000000000
-  let templateVars = { urls: urlDatabase, title: 'TinyApp', user: user_id }; // TODO: MAKE DYNAMIC!!!
-  // TROUBLESHOOTING: let templateVars = { urls: urlDatabase IS OKAY 000000000000
+  let email = request.session.user_id;
+  let templateVars = { urls: urlDatabase, title: 'TinyApp', email: email};
   response.render('urls_index', templateVars);
-  // TROUBLESHOOTING: THE ABOVE IS OKAY
 });
 
 // show the user the form to create a new short url
 app.get('/urls/new', (request, response) => {
-// TROUBLESHOOTING: THE ABOVE IS OKAY  
-  let user_id = request.session.user_id;
-// ?????????????????????????????????????????????????????????????????
-  let templateVars = { user: user_id };
+  let email = request.session.user_id;
+  let templateVars = { email: email };
   response.render('urls_new', templateVars);
-  // TROUBLESHOOTING: response.render('urls_new' IS OKAY
 });
 
-// get shortURL NEEDS MORE
-app.get('/urls/:id', (request, response) => {
-// TROUBLESHOOTING: THE ABOVE IS OKAY  
-  let user_id = request.session.user_id;
-  // ABOVE NEEDS TROUBLESHOOTING 000000000000000000
-  let templateVars = { shortURL: request.params.id, longURL: urlDatabase[request.params.id], user: user_id };
-  // TROUBLESHOOTING: let templateVars = { shortURL: request.params.id, IS OKAY 0000000000
+// display shortURL and longURL
+app.get('/urls/:id', (request, response) => {  
+  console.log(request.params)
+  let email = request.session.user_id;
+  let templateVars = { shortURL: request.params.id, longURL: urlDatabase[request.params.id], email: email };
   response.render('urls_show', templateVars);
 });
 
 // Creates a registration page
 app.get('/register', (request, response) => {
   let templateVars = { title: "Register"}
-  //let user_id = request.body.user_id;
-  //let password = request.body.password;
   response.render('register', templateVars);
 });
-// ?????????????????????????????????????????????????????????????????
 
 // login page
 app.get('/login', (request, response) => {
   let templateVars = { title: 'Login'}
-  let user_id = request.body.user_id;
-  let password = request.body.password;
   response.render('login', templateVars);
 });
-// ?????????????????????????????????????????????????????????????????
 
 // public-facing link that turns magically into the full longURL
 app.get('/u/:shortURL', (request, response) => {
@@ -112,52 +97,40 @@ app.get('/u/:shortURL', (request, response) => {
   let longURL = urlDatabase[shortURL];
   response.redirect(longURL);
 });
-// TROUBLESHOOTING: THE ABOVE IS OKAY 
+
+// 
+app.get('/logout', (request, response) => {
+  response.clearCookie('session');
+  response.redirect('/urls');
+});
 
 // registration
 app.post('/register', (request, response) => {
 
-  let user_id = request.body.email;
+  let email = request.body.email;
   let password = request.body.password;
   let userId = generateRandomString(6);
-  userDb[userId] = {id: userId, user: user_id, password: password};
-  // console.log(user, password);
-  // response.cookie(username, request.body.username);
+  userDb[userId] = {id: userId, email: email, password: password};
   request.session.user_id = userId;
   response.redirect('/urls');
 });
-// ?????????????????????????????????????????????????????????????????
 
-// Login
+// validate login
 app.post('/login', (request, response) => {
   // verify user_id === userDb, if true, next step | if false, send error
-  let user_id = request.body.email;
+  let email = request.body.email;
   for (let key in userDb) {
-    if (userDb[key].email === user_id) {
+    if (userDb[key].email === email) {
       // verify correct password, if true, next step | if false, send error
       if (userDb[key].password === request.body.password) {
         // set user_id cookie on successfull login
-        request.session.user_id = user_id
+        request.session.user_id = email
         response.redirect('/urls');
+        return;
       }
-    } 
-  }  
+    }
+  }
   response.redirect(400, '/login');
-});
-
-//  app.post('/login', (request, response) => {
-//    let user_id = request.body.user_id;
-//    request.session(user_id, request.body.user_id);
-//    response.redirect('/urls');
-// The above two lines may or may not be needed.
-
-// TROUBLESHOOTING ABOVE D2D3 COOKIES IN EXPRESS TASK 3
-
-// the login form is in the _header partial
-app.post('/logout', (request, response) => {
-  let user_id = request.body.user_id;
-  response.clearCookie(user_id, request.body.user_id);
-  response.redirect('/urls');
 });
 
 // Actually does the URL-shortening:
@@ -171,8 +144,8 @@ app.post('/urls', (request, response) => {
   urlDatabase[shortURL] = request.body.longURL;
   // console.log(urlDatabase);
   // TODO: we're ghosting on the user's browser.  stop ghosting, tell them where to go next.
+  response.redirect('/urls');
 });
-// TROUBLESHOOTING: ABOVE SHOULD BE OKAY, BUT NOT SURE 0000000000000000000
 
 // route to /urls after updating short url
 app.post('/urls/:id', (request, response) => {
@@ -181,7 +154,6 @@ app.post('/urls/:id', (request, response) => {
   console.log(urlDatabase);
   response.redirect('/urls');
 });
-// TROUBLESHOOTING: THE ABOVE IS OKAY 
 
 // parameters sent with 
 app.post('/urls/:id/delete', function(request, response) {
@@ -191,10 +163,7 @@ app.post('/urls/:id/delete', function(request, response) {
   // console.log(urlDatabase);
   response.redirect('/urls');
 });
-// TROUBLESHOOTING: THE ABOVE IS OKAY 
 
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
 });
-
-// console.log("the bottom");
