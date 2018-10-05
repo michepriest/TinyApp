c99096e5169f38dec64b87454f909067c2035ed3
michepriest <miche.priest@gmail.com>
2018-10-05 04:54:39 +0000

    changed 'email' to 'userID' or 'user' where appropriate for clarification. Also created user specific feature 'Only registered users can shorten URLs'

--- express_server.js
+++ express_server.js
@@ -17,9 +17,21 @@ app.use(cookieSession({
 
 const PORT = 8080;
 const urlDatabase = {
-  'b2xVn2': 'https://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.com',
-  'U7gSzm': 'http://www.twitter.com'
+  'b2xVn2': {
+    shortURL: 'b2xVn2',
+    longURL: 'https://www.lighthouselabs.ca',
+    userID: 'userRandomID',
+  },
+  '9sm5xK': {
+    shortURL: '9sm5xK',
+    longURL: 'http://www.google.com',
+    userID: 'user3RandomID',
+  },
+  'U7gSzm': {
+    shortURL: 'U7gSzm',
+    longURL: 'http://www.twitter.com',
+    userID: 'user3RandomID',
+  },
 };
 
 const userDb = {
@@ -40,6 +52,20 @@ const userDb = {
   }
 };
 
+// specific user's URLs list, inspired by top anser on https://stackoverflow.com/questions/38750705/filter-object-properties-by-key-in-es6
+// let usersURLs = ['urlDatabase.id'];
+
+
+function getUrlsForUser(userID){
+  var result = [];
+  for(var key in urlDatabase){
+    if(urlDatabase[key].userID === userID){
+      result.push(urlDatabase[key]);
+    }
+  }
+  return result;
+};
+
 // Used Matt's code...he said he got the base of it from the top response by csharptest https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript 
 // Solution from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
 function generateRandomString(digits) {
@@ -51,30 +77,51 @@ function generateRandomString(digits) {
   return text;
 };
 
+function findUserByEmail(email) {
+  for (let userID in userDb) {
+    let user = userDb[userID];
+    if (user.email === email) {
+      return user;
+    }
+  }
+  return null;
+};
+
 app.get('/urls.json', (request, response) => {
   response.send(urlDatabase);
 });
 
 // list all the URLs (shortened, with delete links)
+function getUrlsForUser(userID){
+// NOTE: TODO use getUrlsForUser function
 app.get('/urls', (request, response) => {
-  let email = request.session.user_id;
-  let templateVars = { urls: urlDatabase, title: 'TinyApp', email: email};
+  let userID = request.session.user_id; // "abcdef"
+  let user = userDb[userID]             // { id: "abcdef", email: "asd@asd", password: "ghjghgj" }
+  let templateVars = { urls: urlDatabase, title: 'TinyApp', user: user };
   //let userList = {email: {'shortURL' : 'longURL'}}; // attempt to create a way for URLs to belong to users
   //userList(userDB);
   response.render('urls_index', templateVars);
 });
 
 // show the user the form to create a new short url
+// NOTE TODO: Are you logged in?
 app.get('/urls/new', (request, response) => {
-  let email = request.session.user_id;
-  let templateVars = { email: email };
+  let userID = request.session.user_id;
+  let user = userDb[userID];
+  let templateVars = { user: user };
   response.render('urls_new', templateVars);
 });
 
 // display shortURL and longURL
+// NOTE: TODO if not logged in respond with error else do the thing we want to do, are you owner? if userID attached to url matched to userID in session
 app.get('/urls/:id', (request, response) => {  
-  let email = request.session.user_id;
-  let templateVars = { shortURL: request.params.id, longURL: urlDatabase[request.params.id], email: email };
+  let userID = request.session.user_id;
+  let user = userDb[userID];
+  let templateVars = { 
+    shortURL: request.params.id, 
+    longURL: urlDatabase[request.params.id], 
+    user: user,
+  };
   response.render('urls_show', templateVars);
 });
 
@@ -105,12 +152,14 @@ app.get('/logout', (request, response) => {
 // registration
 app.post('/register', (request, response) => {
 
+/// NOTE TODO!!!!!: need to check if user exists, use function findUserByEmail
+
   let email = request.body.email;
   let userId = generateRandomString(6);
   let password = request.body.password;
   let hashedPassword = bcrypt.hashSync(password, 10);
   userDb[userId] = {id: userId, email: email, password: hashedPassword};
-  request.session.user_id = email;
+  request.session.user_id = userId;
   console.log(userDb);
   response.redirect('/urls');
 });
@@ -120,24 +169,36 @@ app.post('/login', (request, response) => {
   // verify user_id === userDb, if true, next step | if false, send error
   let email = request.body.email;
   let password = request.body.password;
-  for (let key in userDb) {
-    if (userDb[key].email === email) {
-      // verify correct password, if true, next step | if false, send error
-      let hashedPassword = userDb[key].password;
-      if (bcrypt.compareSync(password, hashedPassword)) {
-        // set user_id cookie on successfull login
-        request.session.user_id = email
-        response.redirect('/urls');
-        return;
-      }
+  let user = findUserByEmail(email);
+  
+  if (user) {
+    // verify correct password, if true, next step | if false, send error
+    let hashedPassword = user.password;
+    if (bcrypt.compareSync(password, hashedPassword)) {
+      // set user_id cookie on successfull login
+      request.session.user_id = user.id;
+      response.redirect('/urls');
+      return;
     }
-  }
+  } 
   response.status(400).send("error logging in");
 });
 
+// app.post('/urls/:id', (request, response) => {
+  
+  //if (// user !== logged in) {
+    // redirect to /login 
+  // }
+    //if (// user logged in
+    // && creates shortURL) {
+    // send user's own shortURLs to their URLs page
+    // }
+
+// })
 
 //   * generates a short, random url
 //   * adds or appends shortURL into the database
+// NOTE: TODO if not logged in respond with error 
 app.post('/urls', (request, response) => {
   let shortURL = generateRandomString(6); 
   urlDatabase[shortURL] = request.body.longURL;
@@ -145,11 +206,12 @@ app.post('/urls', (request, response) => {
 });
 
 // route to /urls after updating short url
+// NOTE: TODO if not logged in respond with error else do the thing we want to do, are you owner? if userID attached to url matched to userID in session
 app.post('/urls/:id', (request, response) => {
   urlDatabase[request.params.id] = request.body.longURL;
   response.redirect('/urls');
 });
-
+// NOTE: TODO if not logged in respond with error else do the thing we want to do, are you owner? if userID attached to url matched to userID in session
 app.post('/urls/:id/delete', (request, response) => {
   let urlId = request.params.id;
   delete urlDatabase[urlId];
